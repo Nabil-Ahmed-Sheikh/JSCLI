d8732ac35ddfae41cfd324a3a2d98b8703c35cfb
Nabil-Ahmed-Sheikh <nabil.ahmednsu@gmail.com>
2024-01-21 14:21:20 -0700

    Update/Change
    - Tool now moves files to audio, doc and image folder

--- tool/src/commands/start.js
+++ tool/src/commands/start.js
@@ -5,34 +5,42 @@ const extension = require('../extensions')
 
 module.exports = function start(config) {
     fs.readdir(process.cwd(), (err, files) => {
+
+        // Create Directories
+        for (dir in extension){
+            makeDirectory(dir)
+        }
+
+        // Move Files
         files.forEach(file => {
             const filePath = path.join(process.cwd(), file)
-            // path.extname(filePath) === desiredExtension
             if (fs.statSync(filePath).isFile()) {
-                
                 if (extension['audio'].includes(path.extname(filePath))) {
-                    logger.highlight('Audio')
+                    moveFile(filePath, path.join(process.cwd(),'audio',file))
                 } else if (extension['image'].includes(path.extname(filePath))) {
-                    logger.highlight('image')
+                    moveFile(filePath, path.join(process.cwd(),'image',file))
                 } else if (extension['doc'].includes(path.extname(filePath))) {
-                    logger.highlight('Doc')
-                } else {
-                    logger.highlight(path.extname(filePath))
+                    moveFile(filePath, path.join(process.cwd(),'doc',file))
                 }
-                
-                // console.log(`${file}`);
             }
         })
         
     })
 
-    logger.highlight('  Starting the app  ')
+    logger.highlight('  Files are successfully moved  ')
     logger.debug('Received configuration', config)
 }
 
 
 function makeDirectory (dirName) {
     if(!fs.existsSync(dirName)) {
+        fs.mkdirSync(dir)
         logger.highlight(`${dirName} Directory created`)
     }
+}
+
+function moveFile (oldPath, newPath) {
+    fs.rename(oldPath, newPath, function (err) {
+        if (err) throw err
+      })
 }
